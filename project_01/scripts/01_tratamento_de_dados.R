5d445b5e4158c535a4f73d3d81582c6461d3c002
Souza284 <kanaan.pedro2@gmail.com>
2026-06-15 22:30:13 -0300

    Importei a base de dados dos moradores

---
+++ project_01/scripts/01_tratamento_de_dados.R
@@ -0,0 +1,8 @@
+# Importando a base de dados ----------------------------------------------
+
+#library(tidyverse)
+
+dados_brutos_moradores <- read.csv("dados/moradores.csv", sep = ";")
+
+View(dados_brutos_moradores)
+

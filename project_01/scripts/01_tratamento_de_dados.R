42bdc75e60a1e6da5995acfc071467af6d8ae1e2
Souza284 <kanaan.pedro2@gmail.com>
2026-06-16 11:04:31 -0300

    selecionei as variaveis e criei o banco de dados

--- project_01/scripts/01_tratamento_de_dados.R
+++ project_01/scripts/01_tratamento_de_dados.R
@@ -1,8 +1,40 @@
 # Importando a base de dados ----------------------------------------------
 
-#library(tidyverse)
+library(tidyverse)
 
 dados_brutos_moradores <- read.csv("dados/moradores.csv", sep = ";")
 
 View(dados_brutos_moradores)
+
+# Manipulando a base de moradores -----------------------------------------
+
+#Variáveis: 
+
+##Localidade - Categórica nominal
+##sexo (E03) - Categórica nominal
+##cor/raça (E05) - Categórica nominal
+##meio de transporte (I09_9) - Categórica nominal
+#tempo gasto no deslocamento pro trabalho (I10) - Quantitativa contínua
+##renda (renda_ind_r) - Quantitativa contínua
+##escolaridade - Categórica ordinal
+##Local de trabalho principal (I08) - Categórica nominal
+
+dados_moradores <- dados_brutos_moradores %>% 
+  select(localidade, I08, I09_9, I10, renda_ind_r, escolaridade, E05, E03) %>% 
+  rename(regiao_administrativa = localidade, 
+         local_de_trabalho = I08, 
+         meio_de_transporte = I09_9,
+         tempo_gasto_deslocamento = I10,
+         renda = renda_ind_r,
+         cor_raca = E05,
+         sexo = E03) %>% 
+  group_by(regiao_administrativa)
+
+#Amostra
+
+View(summarise(dados_moradores, tot = n()))
+
+
+
+View(dados_moradores)
 

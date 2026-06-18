5012a9e7417fc604b42069f98d57551aeaac416f
Souza284 <kanaan.pedro2@gmail.com>
2026-06-18 01:24:09 -0300

    Coloquei as bases de dados de 2021 nos arquivos, cruzei as bases dos moradores com os domicilios e consegui um banco com aproximadamente 19.000 observacoes completas. Tchau cansei

--- project_01/scripts/01_tratamento_de_dados.R
+++ project_01/scripts/01_tratamento_de_dados.R
@@ -2,37 +2,49 @@
 
 library(tidyverse)
 
+?read.csv
+
+#Salvando a base de dados dos moradores.
 dados_brutos_moradores <- read.csv("dados/moradores.csv", sep = ";")
 
 View(dados_brutos_moradores)
 
-# Manipulando a base de moradores -----------------------------------------
 
-#Variáveis: 
+# Variáveis ---------------------------------------------------------------
 
-##Localidade - Categórica nominal
-##sexo (E03) - Categórica nominal
-##cor/raça (E05) - Categórica nominal
-##meio de transporte (I09_9) - Categórica nominal
-#tempo gasto no deslocamento pro trabalho (I10) - Quantitativa contínua
-##renda (renda_ind_r) - Quantitativa contínua
-##escolaridade - Categórica ordinal
-##Local de trabalho principal (I08) - Categórica nominal
+# Regiao administrativa - Categórica nominal (localidade).
+# Local de trabalho - Categórica nominal (I08).
+# Meio de transporte - Categorica nominal (I09_9).
+# Sexo - Categórica nominal (E03).
+# Cor/raca - Categórica nominal (E05).
+# Escolaridade - Categorica ordinal (escolaridade).
+# Tempo - Categorica ordinal (I10).
+# Renda - Quantitativa continua (renda_ind_r).
 
-dados_moradores <- dados_brutos_moradores %>% 
-  select(localidade, I08, I09_9, I10, renda_ind_r, escolaridade, E05, E03) %>% 
-  rename(regiao_administrativa = localidade, 
-         local_de_trabalho = I08, 
-         meio_de_transporte = I09_9,
-         tempo_gasto_deslocamento = I10,
-         renda = renda_ind_r,
+
+# Tratamento e manipulação dos dados --------------------------------------
+
+#Base de dados dados_moradores_tratamento armazena as variáveis e as observações
+#que não apresentam NA em nenhuma coluna.
+
+dados_moradores_tratamento <- dados_brutos_moradores %>% 
+  select(regiao_administrativa = localidade, 
+         local_trabalho = I08,
+         meio_transporte = I09_9,
+         tempo_deslocamento = I10,
+         escolaridade,
          cor_raca = E05,
-         sexo = E03) %>% 
-  group_by(regiao_administrativa)
+         sexo = E03,
+         renda = renda_ind_r) %>% 
+  filter(
+    !regiao_administrativa == 99999,
+    !local_trabalho == 99999,
+    !meio_transporte == 99999,
+    !tempo_deslocamento == 99999,
+    !escolaridade == 99999,
+    !cor_raca == 99999,
+    !sexo == 99999,
+    !renda == 99999
+  )
 
-#Amostra
-
-View(summarise(dados_moradores, tot = n()))
-
-View(dados_moradores)
-
+View(dados_moradores_tratamento)
